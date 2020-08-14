26fadff65aa3d8e720b799a9557d94d73129ca59
Fangzhou Long <me@imvery.moe>
2020-08-14 18:06:16 +0800

    fix redis get chat period bug

--- manager/group_chat_manager_redis.py
+++ manager/group_chat_manager_redis.py
@@ -11,12 +11,14 @@ class GroupChatManagerRedis(GroupChatManager):
     use manager.prefix as the redis key,
     also uses manager.duration for redis expire time."""
 
-    def get_redis_key(self, group_id) -> str:
-        return '{}:{}:{}'.format(self.prefix, group_id, self.get_current_period())
+    def get_redis_key(self, group_id, period) -> str:
+        return '{}:{}:{}'.format(self.prefix, group_id, period)
 
     def add_chat(self, group_id: int) -> int:
-        redis_cache.set(self.get_redis_key(group_id), 0, ex=self.duration, nx=True)
-        return redis_cache.incr(self.get_redis_key(group_id), 1)
+        period = self.get_current_period()
+        redis_cache.set(self.get_redis_key(group_id, period), 0, ex=self.duration, nx=True)
+        return redis_cache.incr(self.get_redis_key(group_id, period), 1)
 
     def get_chat_count(self, group_id: int) -> int:
-        return int(redis_cache.get(self.get_redis_key(group_id)) or 0)
+        period = self.get_current_period(offset=-5)
+        return int(redis_cache.get(self.get_redis_key(group_id, period)) or 0)
